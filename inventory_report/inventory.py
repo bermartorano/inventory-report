060dcec87b40edc31b31da2f144311234da5b6cf
Bernardo Martorano <bernardormartorano@gmail.com>
2023-07-18 16:02:02 -0300

    req05

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: list[Product] | None = None):
+        self.__data = data if data else []
+
+    @property
+    def data(self) -> list[Product]:
+        return self.__data
+
+    def add_data(self, data: list[Product]) -> None:
+        for product in data:
+            self.__data.append(product)
